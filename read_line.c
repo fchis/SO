b9ed41cd6ce00bb1f27c0bb198ee0e693e825826
fchis <felicia_chis_08@yahoo.com>
2016-11-28 15:32:36 +0200

    Add files via upload

---
+++ read_line.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <ctype.h>
+
+
+int main(int argc, char* argv[])
+{
+
+if(argc!=2)
+{
+ printf("Usage: <%s> file",argv[0]);
+}	
+
+FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        exit(1);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu :\n", read);
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+}
